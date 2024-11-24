25cc0705776bff0799dad04e83e3504c7cb6cec8
Celmar John Ortiz <cortiz@csyoutsourcing.com>
2024-11-24 14:13:06 +0800

    changed to prod ip

--- src/services/api/axios/axios-instance.ts
+++ src/services/api/axios/axios-instance.ts
@@ -2,10 +2,10 @@ import axios, { AxiosInstance } from "axios";
 import { encryptPayload } from "../../../utils/common-utils";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:54870/api/web-totolan",
+  baseURL: "http://52.76.47.112:54870/api/web-totolan",
   timeout: 30000,
   headers: {
-    "x-api-secret": "http://localhost:54870/api",
+    "x-api-secret": "http://52.76.47.112:54870/api",
     "X-Frame-Options": "DENY",
     "Content-Security-Policy": "frame-ancestors 'self'",
   },
